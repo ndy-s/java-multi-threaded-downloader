bd305a99c3e5cd4b1076e727df530d3bbb8bc7ff
ndys <hendy.sg02@gmail.com>
2025-03-04 23:21:42 +0700

    [feat] Initial project

---
+++ src/test/java/ndys/maven/AppTest.java
@@ -0,0 +1,19 @@
+package ndys.maven;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
